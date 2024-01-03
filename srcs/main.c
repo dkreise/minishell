4b061549e449b0360249fd94805a0aabf1bb5a9d
Roberto <you@example.com>
2024-01-03 22:46:18 +0100

    testing

--- srcs/main.c
+++ srcs/main.c
@@ -1,9 +1,52 @@
-//#  include <readline/rlstdc.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rpliego <rpliego@student.42barcelo>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/03 22:44:44 by rpliego           #+#    #+#             */
+/*   Updated: 2024/01/03 22:44:48 by rpliego          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#include <readline/readline.h>
+#include "../inc/minishell.h"
 
-int main(void)
+void	print_header(void)
 {
-	readline("holaaa");
+	printf("%s           _       _     _          _ _ %s\n", F, E);
+	printf("%s _ __ ___ (_)_ __ (_)___| |__   ___| | |%s\n", F, E);
+	printf("%s| '_ ` _ || | '_ || / __| '_ | / _ | | |%s\n", F, E);
+	printf("%s| | | | | | | | | | |__ | | | |  __/ | |%s\n", F, E);
+	printf("%s|_| |_| |_|_|_| |_|_|___/_| |_||___|_|_|%s\n\n\n", F, E);
+}
+
+int main(int ac, char **av , char **env)
+{
+	char *line;
+	int	i;
+	int	j;
+
+	(void)ac;
+	(void)av;
+	i = 0;
+	while (env[i] != NULL)
+	{
+		j = 0;
+		while (env[i][j] != '\0')
+		{
+			printf("%c", env[i][j]);
+			j++;
+		}
+		printf("\n");
+		i++;
+	}
+	printf("%i\n", i);
+	while (1)
+	{
+		line = readline("\033[1;33mмини-оболочка-0.1$\033[m ");
+		printf("%s\n", line);
+		add_history(line);
+	}
 	return (0);
 }
