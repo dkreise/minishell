f741e0d9b953dbf45091ab7a7a8a5cccf0652e6d
Diana Kreise <dkreise@car14s5.42barcelona.com>
2024-01-04 17:35:59 +0100

    commented function

--- srcs/parser/errors_parser.c
+++ srcs/parser/errors_parser.c
@@ -6,12 +6,12 @@
 /*   By: dkreise <dkreise@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/04 17:04:49 by dkreise           #+#    #+#             */
-/*   Updated: 2024/01/04 17:30:52 by dkreise          ###   ########.fr       */
+/*   Updated: 2024/01/04 17:35:22 by dkreise          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
-
+/*
 void	free_toklst(t_token **tok)
 {
 	t_token	*prev;
@@ -23,13 +23,13 @@ void	free_toklst(t_token **tok)
 		*tok = (*tok)->next;
 		free(prev);
 	}
-}
+}*/
 
 void	parser_error(char *msg, t_token **tok)
 {
 	ft_putstr_fd("minishell: ", 2);
 	ft_putstr_fd(msg, 2);
 	//free
-	free_toklst(tok);
+	//free_toklst(tok);
 	exit(1);
 }
